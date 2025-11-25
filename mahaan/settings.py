881ab230b0efaf57926af9bc0ef7218ed340e284
mediabigscoopstudio <media.bigscoopstudio@gmail.com>
2025-11-25 20:58:56 +0530

    Production setup: whitelist, static config, requirements.txt

--- mahaan/settings.py
+++ mahaan/settings.py
@@ -125,6 +125,9 @@ STATIC_URL = '/static/'
 STATIC_ROOT = '/var/www/mahaan_exports/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = '/var/www/mahaan_exports/media/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
